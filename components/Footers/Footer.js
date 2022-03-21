f0e82712d9cce35c815bb3db9a07090e942b3366
Dante-Nephilim <67158270+Dante-Nephilim@users.noreply.github.com>
2022-03-21 23:02:34 +0530

    Update Footer.js

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -142,9 +142,9 @@ export default function Footer() {
             </div>
           </div>
           <hr className="my-6 border-blueGray-300" />
-//           <div className="flex flex-wrap items-center md:justify-between justify-center">
-//             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-//               <div className="text-sm text-blueGray-500 font-semibold py-1">
+          <div className="flex flex-wrap items-center md:justify-between justify-center">
+            <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+              <div className="text-sm text-blueGray-500 font-semibold py-1">
 //                 Copyright © {new Date().getFullYear()} Notus NextJS by{" "}
 //                 <a
 //                   href="https://www.creative-tim.com?ref=nnjs-footer"
@@ -152,10 +152,10 @@ export default function Footer() {
 //                 >
 //                   Creative Tim
 //                 </a>
-//                 .
-//               </div>
-//             </div>
-//           </div>
+                .
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
     </>
