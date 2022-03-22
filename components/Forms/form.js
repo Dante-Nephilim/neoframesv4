c2697bde1d5fe94cf0a56751a7261aed0e817e90
Dante-Nephilim <sakkara.ps@gmail.com>
2022-03-22 17:29:46 +0530

    removed button

--- components/Forms/form.js
+++ components/Forms/form.js
@@ -46,14 +46,7 @@ export default function Form(){
       <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
     </div>
   </div>
-  <div className="md:flex md:items-center">
-    <div className="md:w-1/3">
-      <button className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-        Send
-      </button>
-    </div>
-    <div className="md:w-2/3"></div>
-  </div>
+  
 </form>
 <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
   Submit
